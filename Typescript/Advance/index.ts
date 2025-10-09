d7b099750e176ac269a7b098efe7509dd9d66830
Naresh Perumalla <naresh.xplores777@gmail.com>
2025-10-09 15:20:54 +0530

    "Advanced"

--- Typescript/Advance/index.ts
+++ Typescript/Advance/index.ts
@@ -69,3 +69,87 @@ function getFirstElement<T>(arr: T[]): T {
 
 let num = getFirstElement([10, 20, 30]); // num: number
 let str = getFirstElement(["Naresh", "Kiran"]); // str: string
+
+
+
+
+//OBJECTS
+
+let person2: { name: string; age: number };
+
+person2 = {
+  name: "Naresh",
+  age: 22
+};
+
+let user2: { name: string; age?: number };
+
+user2 = { name: "Jonas" }; // ✅ age is optional
+user2 = { name: "Martha", age: 20 }; // ✅ also fine
+
+
+let student: { readonly id: number; name: string } = {
+  id: 101,
+  name: "Naresh"
+};
+
+student.name = "Jonas"; // ✅ allowed
+student.id = 200;       // ❌ Error: Cannot assign to 'id'
+
+
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+let p1: Person = { name: "Naresh", age: 22 };
+let p2: Person = { name: "Martha", age: 21 };
+
+
+
+type Address = {
+  city: string;
+  country: string;
+};
+
+type Person = {
+  name: string;
+  age: number;
+  address: Address;
+};
+
+let p1: Person = {
+  name: "Naresh",
+  age: 22,
+  address: {
+    city: "Hyderabad",
+    country: "India"
+  }
+};
+
+type Student = {
+  name: string;
+  roll: number;
+};
+
+let students: Student[] = [
+  { name: "Jonas", roll: 1 },
+  { name: "Martha", roll: 2 }
+];
+
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+}
+
+let item: Product = {
+  id: 101,
+  title: "Laptop",
+  price: 55000
+};
+
+
+
