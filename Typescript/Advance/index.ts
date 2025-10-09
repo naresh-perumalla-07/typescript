f08d4a59e0c8f43c864f5be87071233ec7367fe9
Naresh Perumalla <naresh.xplores777@gmail.com>
2025-10-09 15:13:06 +0530

    "Advanced"

---
+++ Typescript/Advance/index.ts
@@ -0,0 +1,24 @@
+
+
+//Arrays
+
+let arr:string[];
+arr=["Jonas","Martha"]
+
+let arr2:number[]=[1,2,3,2,3];
+
+let marks: Array<number> = [90, 85, 76];
+let studentss: Array<string> = ["Naresh", "Kiran"];
+
+
+let fruits: string[] = ["Apple", "Banana", "Mango"];
+
+interface Student {
+  name: string;
+  age: number;
+}
+
+let students: Student[] = [
+  { name: "Naresh", age: 21 },
+  { name: "Kiran", age: 22 },
+];
