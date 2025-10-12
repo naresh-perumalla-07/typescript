995b149648ee9587b08d310f6864e741cf803d98
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-10-12 21:29:30 +0530

    Update index.ts

--- Typescript/Basics/index.ts
+++ Typescript/Basics/index.ts
@@ -21,7 +21,7 @@ if (isLoggedIn) {
   console.log("Please login");
 }
 // The any type disables all type checking for that variable.
-// It basically tells TypeScript:bgdhhhhddggdf
+// It basically tells TypeScript:bgdhhhhddggdfbbf
 
 // “Trust me, I know what I’m doing — don’t check this!”
 
